f3278bcd711e6923432af4fe04e07f4f69360a39
Nasir Alauddin <nalaudd@yahoo.com>
2018-01-14 10:24:57 -0500

    Made person bean

---
+++ ArcherStudioMVN/src/main/java/alauddin/archerstudio/beans/Person.java
@@ -0,0 +1,41 @@
+package alauddin.archerstudio.beans;
+
+import java.sql.Date;
+
+public class Person {
+	
+	String username, fname, lname;
+	int age;
+	Date birthday;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	
+}
