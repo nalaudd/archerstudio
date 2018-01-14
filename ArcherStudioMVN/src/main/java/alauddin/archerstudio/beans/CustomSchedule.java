7d8846c38ba895fa724fb886308bd792568d799e
Nasir Alauddin <nalaudd@yahoo.com>
2018-01-14 10:34:40 -0500

    Made beans for user and custom/work schedule

---
+++ ArcherStudioMVN/src/main/java/alauddin/archerstudio/beans/CustomSchedule.java
@@ -0,0 +1,61 @@
+package alauddin.archerstudio.beans;
+
+public class CustomSchedule {
+	String sun, mon, tue, wed, thu, fri, sat;
+
+	public String getSun() {
+		return sun;
+	}
+
+	public void setSun(String sun) {
+		this.sun = sun;
+	}
+
+	public String getMon() {
+		return mon;
+	}
+
+	public void setMon(String mon) {
+		this.mon = mon;
+	}
+
+	public String getTue() {
+		return tue;
+	}
+
+	public void setTue(String tue) {
+		this.tue = tue;
+	}
+
+	public String getWed() {
+		return wed;
+	}
+
+	public void setWed(String wed) {
+		this.wed = wed;
+	}
+
+	public String getThu() {
+		return thu;
+	}
+
+	public void setThu(String thu) {
+		this.thu = thu;
+	}
+
+	public String getFri() {
+		return fri;
+	}
+
+	public void setFri(String fri) {
+		this.fri = fri;
+	}
+
+	public String getSat() {
+		return sat;
+	}
+
+	public void setSat(String sat) {
+		this.sat = sat;
+	}
+}
