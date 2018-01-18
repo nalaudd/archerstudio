0d7eff6bd9b9fd079160e7171e20a02af6949e26
Nasir Alauddin <nalaudd@yahoo.com>
2018-01-18 08:58:58 -0500

    added persistance services

---
+++ ArcherStudioMVN/src/main/java/alauddin/archerstudio/services/UpdateInfoService.java
@@ -0,0 +1,51 @@
+package alauddin.archerstudio.services;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import alauddin.archerstudio.beans.User;
+import alauddin.archerstudio.util.HibernateUtil;
+
+@Path("/User")
+public class UpdateInfoService {
+	
+	public static Session session = new HibernateUtil().getSession();
+	
+	/**
+	 * Takes an input of a user object and updates its changes in the
+	 * table
+	 * @param user
+	 */
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/update")
+	public void update(User user){
+		Transaction tx = session.beginTransaction();
+		session.update(user);
+		tx.commit();
+		session.close();
+	}
+	
+	/**
+	 * Takes the user objects and saves its information into the appropriate tables as a new
+	 * user.
+	 * @param user
+	 */
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/create")
+	public void createUser(User user){
+		Transaction tx = session.beginTransaction();
+		session.save(user);
+		session.save(user.getPerson());
+		session.save(user.getCs());
+		session.save(user.getWs());
+		tx.commit();
+		session.close();
+	}
+}
