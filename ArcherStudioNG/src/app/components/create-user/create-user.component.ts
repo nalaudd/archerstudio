f195d297e8124c796afb33a69c8f4f0cef05ffb1
Nasir Alauddin <nalaudd@yahoo.com>
2018-01-22 10:52:30 -0500

    user creation model implemented

--- ArcherStudioNG/src/app/components/create-user/create-user.component.ts
+++ ArcherStudioNG/src/app/components/create-user/create-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserCreation } from '../../models/UserCreation';
 
 @Component({
   selector: 'app-create-user',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-user.component.css']
 })
 export class CreateUserComponent implements OnInit {
+  
+  uc: UserCreation;
+  password: string;
+  birthday: string;
+  month: number;
+  day: number;
+  year: number;
 
   constructor() { }
 
   ngOnInit() {
+    this.uc = new UserCreation();
+  }
+
+  register(){
+    if (this.uc.password !== this.password){
+      alert("Passwords do not match!");
+    } else {
+      this.birthday = this.year + '-' + this.month + '-' + this.day;
+      this.uc.birthday = this.birthday;
+      console.log(this.uc);
+    }
+    
   }
 
 }
---
+++ ArcherStudioNG/src/app/models/UserCreation.ts
@@ -0,0 +1,8 @@
+export class UserCreation {
+    username: string;
+    password: string;
+    fname: string;
+    lname: string;
+    birthday: string;
+    age: number;
+}
