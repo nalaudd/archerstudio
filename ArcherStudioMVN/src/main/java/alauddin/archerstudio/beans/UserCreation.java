e6d5a9557c7c831895c2dab90b96c2cb4378eb69
Nasir Alauddin <nalaudd@yahoo.com>
2018-01-22 08:12:32 -0500

    made user creation bean

---
+++ ArcherStudioMVN/src/main/java/alauddin/archerstudio/beans/UserCreation.java
@@ -0,0 +1,47 @@
+package alauddin.archerstudio.beans;
+
+import java.sql.Date;
+
+public class UserCreation {
+	String fname, lname, username, password;
+	int age;
+	Date birthday;
+	
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	
+}
