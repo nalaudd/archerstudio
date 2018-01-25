09f00a44f9fc2b882ccfefaaa0c32c1b99549e6a
Nasir Alauddin <nalaudd@yahoo.com>
2018-01-25 09:32:10 -0500

    users can now search for other schedules

--- ArcherStudioNG/src/app/components/other-schedule/other-schedule.component.ts
+++ ArcherStudioNG/src/app/components/other-schedule/other-schedule.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchScheduleService } from '../../services/search-schedule/search-schedule.service';
+import { Schedule } from '../../models/Schedule';
+import { CustomSchedule } from '../../models/CustomSchedule';
+import { WorkSchedule } from '../../models/WorkSchedule';
 
 @Component({
   selector: 'app-other-schedule',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OtherScheduleComponent implements OnInit {
 
-  constructor() { }
+  userId: number;
+  schedule: Schedule;
+
+  constructor(private scheduleSearch: SearchScheduleService) { }
 
   ngOnInit() {
+    this.schedule = new Schedule();
+    this.schedule.ws = new WorkSchedule();
+    this.schedule.cs = new CustomSchedule();
+  }
+
+  search(){
+    this.scheduleSearch.search(this.userId).subscribe( data =>{
+      this.schedule = data.json();
+    });
   }
 
 }
--- ArcherStudioNG/src/app/services/search-schedule/search-schedule.service.ts
+++ ArcherStudioNG/src/app/services/search-schedule/search-schedule.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class SearchScheduleService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  search(userId: number){
+    return this.http.get('http://localhost:8085/ArcherStudioMVN/rest/UserInfo/Schedule/' + userId);
+  }
 
 }
